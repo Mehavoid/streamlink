8d11f57cf882d076f8f52eadb9134b57855d8311
Mehavoid <tontole@riseup.net>
2022-03-09 00:32:51 +0100

    Init plugin template

---
+++ src/streamlink/plugins/trovo.py
@@ -0,0 +1,19 @@
+"""
+$url trovo.live
+$type live, vod
+"""
+
+import logging
+import re
+
+from streamlink.plugin import Plugin, pluginmatcher
+
+log = logging.getLogger(__name__)
+
+
+class Trovo(Plugin):
+    def __init(self, url):
+        pass
+
+
+__plugin__ = Trovo
