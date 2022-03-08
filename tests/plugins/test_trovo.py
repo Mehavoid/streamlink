cb37bf5f722c364bb6f326d6598050b540fb63c1
Mehavoid <tontole@riseup.net>
2022-03-09 00:49:28 +0100

    fixup! Init test cases

--- tests/plugins/test_trovo.py
+++ tests/plugins/test_trovo.py
@@ -7,17 +7,17 @@ class TestPluginCanHandleUrlTrovo(PluginCanHandleUrl):
 
     should_match_groups = [
         ('https://trovo.live/Maddyson',
-            ('channel', 'Maddyson')),
+            {'channel': 'Maddyson'}),
         ('https://trovo.live/Maddyson?adtag=user.EXAMPLE.clip',
-            ('channel', 'Maddyson')),
+            {'channel': 'Maddyson'}),
         ('https://trovo.live/clip/lc-387743296843423010',
-            ('video', 'lc-387743296843423010')),
+            {'video': 'lc-387743296843423010'}),
         ('https://trovo.live/clip/lc-387702296843423010?ltab=videos',
-            ('video', 'lc-387702296843423010')),
+            {'video': 'lc-387702296843423010'}),
         ('https://trovo.live/video/ltv-108024053_108024053_387702296935586449',
-            ('video', 'ltv-108024053_108024053_387702296935586449')),
+            {'video': 'ltv-108024053_108024053_387702296935586449'}),
         ('https://trovo.live/video/ltv-108024053_108024053_387702296935586449?ltab=videos&adtag=user.EXAMPLE.clip',
-            ('video', 'ltv-108024053_108024053_387702296935586449')),
+            {'video': 'ltv-108024053_108024053_387702296935586449'}),
     ]
 
     should_not_match = [
