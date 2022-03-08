105f39a27a70d4da28896809e24055ec2aefcb2d
Mehavoid <tontole@riseup.net>
2022-03-09 00:44:01 +0100

    Add pluginmatcher

--- src/streamlink/plugins/trovo.py
+++ src/streamlink/plugins/trovo.py
@@ -11,6 +11,14 @@ from streamlink.plugin import Plugin, pluginmatcher
 log = logging.getLogger(__name__)
 
 
+@pluginmatcher(re.compile(r"""
+    https?://trovo\.live/
+    (?:
+        (?:clip|video)/(?P<video>[-_ltvc0-9]+)
+        |
+        (?P<channel>[^/?]+)
+    )
+""", re.VERBOSE))
 class Trovo(Plugin):
     def __init(self, url):
         pass
